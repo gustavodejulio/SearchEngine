e7204fa1b5daa7891280ef49cec0739dd25a79c4
gustavodejulio <60690787+gustavodejulio@users.noreply.github.com>
2022-10-22 12:19:31 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
-SEARCH_KEY = "AIzaSyCbTDf4uuElbgvCCYbr38Ghb0KXfX6Gck4"
-SEARCH_ID = "45a165c24427544fa"
+SEARCH_KEY = "YOUR API KEY"
+SEARCH_ID = "YOUR SEARCH ID"
 COUNTRY = "us"
 SEARCH_URL = "https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&q={query}&start={start}&gl=" + COUNTRY
 RESULT_COUNT = 20
